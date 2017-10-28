c8fc966c56121dd1248423d81c1042297254909e
chrisziegler <chrisrziegler@gmail.com>
2017-10-27 18:21:57 -0700

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@ var mongoose = require('mongoose');
 //use standard built-in ES6 Promises
 mongoose.Promise = global.Promise;
 // path/ip still provided statically - and revealed in mongodb server command prompt
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };
