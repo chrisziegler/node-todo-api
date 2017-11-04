66a1342ae778c8bc3c6936c21bcbef24d80b268c
chrisziegler <chrisrziegler@gmail.com>
2017-11-03 19:00:11 -0700

    POST /users e-mail validation and unique, and password defaults

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,41 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+
 
 var User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
-        //prevents empty string input case
         minLength: 1,
-        //trim off leading or trailing whitespace
-        trim: true
+        trim: true,
+        //checks to make sure the property email does not have the same value as any
+        //other documents in the collection - e.g. the user has already signed-up
+        unique: true,
+        validate: {
+            //same as
+            // validator: (value) => {
+            //     return validator.isEmail(value);
+            // },
+            validator: validator.isEmail,
+            //mongoose syntax for {INJECT}
+            message: '{VALUE} is not a valid email'
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6
+        },
+        tokens: [{
+            access: {
+                type: String,
+                required: true
+            },
+            token: {
+                type: String,
+                required: true
+            }
+        }]
     }
 });
 
